98b6fe6e46c1e0bdf3aa338e6cfb3889ffab9cca
Minh Nguyen <minhnguyen@email.arizona.edu>
2019-01-30 13:23:06 -0700

    handles filenotfound exception

--- CipherCrack.py
+++ CipherCrack.py
@@ -1,9 +1,16 @@
 def init():
-    filename = input("Please enter the filename: ")
-    if (not filename.endswith(".txt")):
-        filename += ".txt"
-    ciphertext = open(filename).read()
-    return ciphertext
+    while True:
+        filename = input("Please enter the filename: ")
+        if (not filename.endswith(".txt")):
+            filename += ".txt"
+        
+        try:
+            ciphertext = open(filename).read()
+        except FileNotFoundError:
+            print("File not found.")
+            continue
+        
+        return ciphertext
     
 
 def main():
