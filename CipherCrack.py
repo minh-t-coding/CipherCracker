8c91a174819ad27ce04df811712f33e971359912
Minh Nguyen <minhnguyen@email.arizona.edu>
2019-01-31 09:03:01 -0700

    working IC calculator

--- CipherCrack.py
+++ CipherCrack.py
@@ -1,3 +1,12 @@
+def IC(ctext):
+    summation = 0
+    N = len(ctext)
+    for letter in range(ord('A'),ord('Z')+1):
+        ni = ctext.count(chr(letter))
+        summation += (ni*(ni-1))
+        
+    return (summation * (1/(N*(N-1))))
+
 def init():
     while True:
         filename = input("Please enter the filename: ")
@@ -5,17 +14,18 @@ def init():
             filename += ".txt"
         
         try:
-            ciphertext = open(filename).read()
+            ctext = open(filename).read()
         except FileNotFoundError:
             print("File not found.")
             continue
         
-        return ciphertext
+        return ctext
     
 
 def main():
-    ciphertext = init()
-    print(ciphertext)
+    ctext = init()
+    print(ctext)
+    print(IC(ctext))
     return
 
 main()
