25a4a4d0788efc166c764c61b9d6c25c92dac244
Minh Nguyen <minhnguyen@email.arizona.edu>
2019-01-31 09:16:53 -0700

    started freqAnalysis

--- CipherCrack.py
+++ CipherCrack.py
@@ -1,12 +1,3 @@
-def IC(ctext):
-    summation = 0
-    N = len(ctext)
-    for letter in range(ord('A'),ord('Z')+1):
-        ni = ctext.count(chr(letter))
-        summation += (ni*(ni-1))
-        
-    return (summation * (1/(N*(N-1))))
-
 def init():
     while True:
         filename = input("Please enter the filename: ")
@@ -21,11 +12,29 @@ def init():
         
         return ctext
     
+def IC(ctext):
+    #returns the Index of Coincidence
+    summation = 0
+    N = len(ctext)
+    for letter in range(ord('A'),ord('Z')+1):
+        ni = ctext.count(chr(letter))
+        summation += (ni*(ni-1))
+        
+    return (summation * (1/(N*(N-1))))
 
+def freqAnal(ctext):
+    #returns frequencies of monograms and digrams
+    freqTable = {}
+    for letter in range(ord('A'),ord('Z')+1):
+        freqTable[chr(letter)] = ctext.count(chr(letter))
+    #TODO: Digrams??
+    return freqTable
+    
 def main():
     ctext = init()
     print(ctext)
     print(IC(ctext))
+    print(freqAnal(ctext))
     return
 
 main()
